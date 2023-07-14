c80f6b4799501a824d9fb587db6ce7e29ce0ef54
Shashi Kant <iamshashikant1301@gmail.com>
2023-07-14 09:47:13 +0530

    addingseconfile

--- app1.js
+++ app1.js
@@ -1,1 +1,6 @@
-console.log('Hello World');
+var items=document.getElementsByClassName('list-group-item');
+items[2].style.backgroundColor='green';
+for(var i=0;i<items.length;i++){
+    items[i].style.fontWeight='bold';
+
+}
